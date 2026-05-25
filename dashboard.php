408de7227aed093764e9fe398fe588014f7e72be
Nidhikandpal <kandpalnidhi410@gmail.com>
2026-05-25 10:23:19 +0530

    Add files via upload

--- dashboard.php
+++ dashboard.php
@@ -1,6 +1,15 @@
 <?php
+session_start();
 
 include 'db.php';
+
+$user_id = $_SESSION['user_id'];
+
+$result = mysqli_query($conn,
+
+"SELECT * FROM transactions
+WHERE user_id='$user_id'
+ORDER BY id DESC");
 
 $result = mysqli_query($conn,
 "SELECT * FROM transactions ORDER BY id DESC");
